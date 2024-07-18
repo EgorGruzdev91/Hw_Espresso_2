1360ac5ae1e0deff4c83e9486d74ea40f2d10301
Egor Gruzdev <egor.gruzdev91@gmail.com>
2024-07-18 16:48:14 +0300

    Completed task 2

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
@@ -10,15 +10,20 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
 import static org.hamcrest.Matchers.allOf;
 
 import android.content.Intent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,7 +36,7 @@ public class MainActivityTest {
     public IntentsTestRule intentsTestRule =
             new IntentsTestRule(MainActivity.class);
     @Test
-    public void mainActivityTest1() {
+    public void mainActivityTest2() {
         ViewInteraction overflowMenuButton = onView(withContentDescription("More options"));
         overflowMenuButton.check(matches(isDisplayed()));
         overflowMenuButton.perform(click());
@@ -43,5 +48,24 @@ public class MainActivityTest {
 
         intended(hasData("https://google.com"));
         intended(hasAction(Intent.ACTION_VIEW));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 }
