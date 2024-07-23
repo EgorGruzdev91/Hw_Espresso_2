b78bcc636ff25fcd7d520f1b2762e20d631c27b3
Egor Gruzdev <egor.gruzdev91@gmail.com>
2024-07-23 15:49:06 +0300

    Completed task2

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/MainActivityTest.java
@@ -36,7 +36,7 @@ public class MainActivityTest {
     public IntentsTestRule intentsTestRule =
             new IntentsTestRule(MainActivity.class);
     @Test
-    public void mainActivityTest2() {
+    public void mainActivityTest() {
         ViewInteraction overflowMenuButton = onView(withContentDescription("More options"));
         overflowMenuButton.check(matches(isDisplayed()));
         overflowMenuButton.perform(click());
@@ -48,24 +48,6 @@ public class MainActivityTest {
 
         intended(hasData("https://google.com"));
         intended(hasAction(Intent.ACTION_VIEW));
-    }
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
     }
 }
